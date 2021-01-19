b6445c78913cb54c71adbb6f50c4a83afe772aec
Christian Anderson <christian.p.anderson1@gmail.com>
2021-01-18 23:47:22 -0500

    cleaned up some code

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) => {
-    //   // example of destructuring
-    //   return React.createElement("div", {}, [
-    //     React.createElement("h1", {}, name),
-    //     React.createElement("h2", {}, animal),
-    //     React.createElement("h2", {}, breed),
-    //   ]);
+export default function Pet({ name, animal, breed }) {
 
     return (
         <div>
